af299ed76a27014caf82963088ae0ce663d9164b
ulieth <uliana.eth@proton.me>
2024-11-07 23:41:12 +0300

    adding emotion_recognition.py file for Neural Network Architecture

---
+++ src/emotion_recognition.py
@@ -0,0 +1,11 @@
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
